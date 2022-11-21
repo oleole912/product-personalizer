aba64ce4a956a6ee6d5d8970069cbcb66dd5cfea
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-11-21 22:15:49 +0000

    Set Button's className prop as optional

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,12 +3,12 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
 const Button = ({children, className}) => {
-    return (<button className={clsx(styles.button, className)}>{children}</button>);
+    return (<button className={clsx(styles.button, className && className)}>{children}</button>);
 };
 
 Button.propTypes = {
     children: PropTypes.any.isRequired,
-    className: PropTypes.string.isRequired
+    // className: PropTypes.string.isRequired
 }
 
 export default Button;
